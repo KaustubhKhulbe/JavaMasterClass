b06b0dcedab5c58b8a4d617043fc04ba3662af89
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-08-30 13:58:09 -0700

    Add files via upload
    Working with integers, min and max value testing

---
+++ PrimitiveTypes.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer minimum value = " + myMinIntValue);
+        System.out.println("Integer maximum value = " + myMaxIntValue);
+
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+    }
+}
