d502ad1598e836cd046bfdd14519383503154e3d
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-08-30 19:25:22 -0700

    Add files via upload

---
+++ ByteShortLongWidth.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class ByteShortLongWidth {
+
+    public static void main(String[] args){
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer minimum value = " + myMinIntValue);
+        System.out.println("Integer maximum value = " + myMaxIntValue);
+
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println(myMinByteValue);
+        System.out.println(myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MIN_VALUE;
+
+        System.out.println(myMinShortValue);
+        System.out.println(myMaxShortValue);
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println(myMinLongValue);
+        System.out.println(myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue/2);
+
+    }
+
+}
