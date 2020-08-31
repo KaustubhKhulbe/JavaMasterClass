d614efd226d284866444a65945c6996e202f1ecb
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-08-30 21:41:41 -0700

    Add files via upload
    Converting pounds to kilograms challenge

---
+++ PoundsToKilosChallenge.java
@@ -0,0 +1,15 @@
+public class PoundsToKilosChallenge {
+
+    public static void main(String[] args){
+
+        double numOfPounds = 200d;
+        double kilosConverted = numOfPounds * 0.45359237d;
+
+        System.out.println(kilosConverted);
+
+        double pi = 3.1415927;
+        double anotherNumber = 3_000_000.4_56_7;
+
+    }
+
+}
