aa52db35b3d1b8c05a777aa80bae072379289531
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-08-30 23:04:53 -0700

    Add files via upload
    Booleans and chars

---
+++ CharAndBoolean.java
@@ -0,0 +1,22 @@
+public class CharAndBoolean {
+
+    public static void main(String[] args){
+
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+
+        char myCopyRightChar = '\u00A9';
+        System.out.println(myCopyRightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+
+    }
+
+}
