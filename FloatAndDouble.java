92339a5e2af88d8e7d29230b0549d105dd3c9354
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-08-30 19:41:22 -0700

    Add files via upload
    Floats and doubles in java

---
+++ FloatAndDouble.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class FloatAndDouble {
+
+    public static void main(String[] args) {
+	// write your code here
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("Min float value = " + myMinFloatValue);
+        System.out.println("max float value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Min double value = " + myMinDoubleValue);
+        System.out.println("max double value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+    }
+}
