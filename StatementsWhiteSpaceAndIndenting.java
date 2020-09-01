7c9e915135bdf40f2aca0d0c4c985107f426db5d
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-09-01 13:57:53 -0700

    Add files via upload

---
+++ StatementsWhiteSpaceAndIndenting.java
@@ -0,0 +1,23 @@
+public class StatementsWhiteSpaceAndIndenting {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+
+        if (myVariable == 50) {
+            System.out.println("Printed");
+        }
+
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                " Another test");
+
+        int anotherVarible = 5;
+        myVariable--;
+        System.out.println("This is another one");
+
+    }
+
+}
