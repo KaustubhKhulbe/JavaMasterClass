3654f33307496f8c2eaa9d6274c1dd9b560b974e
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-09-01 10:42:59 -0700

    Add files via upload
    Experimenting with more keywords/expressions

---
+++ KeywordsAndExpressions.java
@@ -0,0 +1,21 @@
+public class KeywordsAndExpressions {
+
+    public static void main(String[] args){
+
+        //1 mile = 1.609344 kilometers
+        double kilometer = (100 * 1.609344);
+
+        int highScore = 50;
+        if(highScore == 50){
+            System.out.println("This is an expression");
+        }
+
+        int score = 100; //expression
+        if(score > 99){ //expression
+            System.out.println("You got the high score!"); //expression
+            score = 0; //expression
+        }
+
+    }
+
+}
