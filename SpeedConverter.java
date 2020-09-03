d60b9e9629f30f1d583787f2b8003d424e85a1e5
KaustubhKhulbe <66220294+KaustubhKhulbe@users.noreply.github.com>
2020-09-02 17:46:25 -0700

    Add files via upload
    Speed control coding excercise

---
+++ SpeedConverter.java
@@ -0,0 +1,24 @@
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) return -1;
+        else return Math.round(kilometersPerHour / 1.609d);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        long milesPerHour = toMilesPerHour(kilometersPerHour);
+        System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+
+
+    }
+
+}
